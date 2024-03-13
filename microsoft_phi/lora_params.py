e9e747d2caad40f1632c9568d3c313da2b510177
ilyas <oulkaddailyas@gmail.com>
2024-03-13 23:11:27 +0100

    updated lora for starcoderbase 1b and phi-2

--- microsoft_phi/lora_params.py
+++ microsoft_phi/lora_params.py
@@ -1,28 +1,17 @@
-from transformers import AutoModelForSeq2SeqLM
 from peft import get_peft_model, LoraConfig, TaskType
 from transformers import AutoModelForCausalLM
+import os
 
 # Set your own token or use another method like login()
-token = 'your_token_here';
-
-# checkpoint = "bigcode/starcoder"
-checkpoint = "bigcode/starcoderbase-3b"
-# checkpoint = "codellama/CodeLlama-7b-hf"
+token = os.environ.get("TOKEN")
+# checkpoint = "bigcode/starcoderbase-1b"
+checkpoint = "microsoft/phi-2"
 
 peft_config = LoraConfig(
     task_type=TaskType.SEQ_2_SEQ_LM, inference_mode=False, r=8, lora_alpha=32, lora_dropout=0.1
 )
 
-model_star = AutoModelForCausalLM.from_pretrained(checkpoint, token=token)
-# model_llama = AutoModelForCausalLM.from_pretrained(llama, token=token)
 
-
-
-model_star = get_peft_model(model_star, peft_config)
-# model_llama = get_peft_model(model_llama, peft_config)
-
-
-print('StarCoder model')
-model_star.print_trainable_parameters()
-# print('Llama model')
-# model_llama.print_trainable_parameters()
+model = AutoModelForCausalLM.from_pretrained(checkpoint, token=token)
+model = get_peft_model(model, peft_config)
+model.print_trainable_parameters()
